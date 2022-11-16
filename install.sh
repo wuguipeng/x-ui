98782b68f35e9dbccf886672f06fe79f4c6c7121
wuguipeng <xyxdbp2022@gmail.com>
2022-11-16 17:32:23 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -133,7 +133,7 @@ install_x-ui() {
         rm /usr/local/x-ui/ -rf
     fi
 
-    tar zxvf x-ui-linux-${arch}.tar.gz
+    tar -xvpf x-ui-linux-${arch}.tar.gz
     rm x-ui-linux-${arch}.tar.gz -f
     cd x-ui
     chmod +x x-ui
